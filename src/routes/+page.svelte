3c1ec70082762c9194ec471ef8318991fc97e764
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-03-20 16:39:47 +0100

    .

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { onMount } from 'svelte';
+    var x = {title: ''}
+    onMount( v => {
+        fetch("http://localhost:3000/posts/2").then(v => v.json()).then(v => x = v).then(console.log)
+    })
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+{x.title}
