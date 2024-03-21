a669c62bf9990d5cdfb626f8b59700fcc02ae170
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-03-21 09:35:43 +0100

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,14 +10,17 @@
 </script>
 <h1>Minimál példa</h1>
 {#each x as {id, title}}
-<i>{id}</i> <input id={id} bind:value={title} on:input={() => {
+<i>{id}</i> <input id={id} bind:value={title} on:change={() => {
+    console.log(id)
     fetch(`http://localhost:3000/posts/${id}`, {
-        method: "PUT",
+        method: "PUT", //Módosít
         body: JSON.stringify({id, title})
-    }).then(v => v.json())
+    }).then(v => v.json()).then(() => {
+        x = x
+    })
 }}/> -- <button on:click={() => {
     fetch(`http://localhost:3000/posts/${id}`, {
-        method: "DELETE"
+        method: "DELETE" //Töröl
     }).then(v => v.json()).then( v => {
         let x2 = []
         x.forEach(i => {
@@ -30,7 +33,7 @@
 {/each}
 <i>{nextid}</i> <input id={nextid} bind:value={newtit} on:change={() => {
     fetch(`http://localhost:3000/posts`, {
-        method: "POST",
+        method: "POST", //Beszúr újat
         headers: { "Content-Type": "application/json"},
         body: JSON.stringify({id: nextid.toString(), title: newtit})
     }).then(v => v.json()).then(() => {
